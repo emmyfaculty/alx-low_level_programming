e0d151d71e6e88fd7c34b529ba889886a5fba12a
emmyfaculty <emmanuelahola91@gmail.com>
2021-08-26 13:25:11 -0700

    correction again

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -6,9 +6,8 @@
  * return: 0 if exited properly, non-zero otherwise
  */
 int main(void)
-{
-        printf("with proper grammar, but the outcome is a piece of art,\n");
+{	
+	printf("with proper grammar, but the outcome is a piece of art,\n");
         return (0);
-
 }
 
