4ecbba68579eb4ee81d52684e7570030e2d118ae
emmyfaculty <emmanuelahola91@gmail.com>
2021-08-31 16:39:37 -0700

    commit

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,18 +1,22 @@
 #include "main.h"
 /**
- * main - Entry point
+ * main - calls the function _putchar
  *
- * Return: Always 0 (Success)
+ * Description: programs that prints Holberton, followed by a new line
+ * Return: 0
  */
+
 int main(void)
 {
-	char *sh = "Holberton";
-
-	while (*sh)
-	{
-		_putchar(*sh);
-		sh++;
-	}
+	_putchar('H');
+	_putchar('o');
+	_putchar('l');
+	_putchar('b');
+	_putchar('e');
+	_putchar('r');
+	_putchar('t');
+	_putchar('o');
+	_putchar('n');
 	_putchar('\n');
 
 	return (0);
