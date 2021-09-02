6762dea4f2078367d1636bf56b0037a6c1c329aa
emmyfaculty <emmanuelahola91@gmail.com>
2021-09-01 23:30:06 -0700

    commiting file

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,10 +1,2 @@
-carrie@ubuntu:/debugging$ cat main.h
-#ifndef MAIN_H
-#define MAIN_H
 
-#include <stdio.h>
-
-void positive_or_negative(int i);
-
-#endif /* MAIN_H */
-carrie@ubuntu:/debugging$ 
+void positive_or_negative(int i); 
