c122aee4a57a81c112fe86dc90af91e10ec73d7f
emmyfaculty <emmanuelahola91@gmail.com>
2021-09-13 16:31:29 -0700

    error

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,17 +1,18 @@
 #include "main.h"
 #include <stdio.h>
-
 /**
- * _memset() functions fills the first n bytes of the memory
- * area pointed to by s with the constant byte b
- * Returns a pointer to the memory area s
+ * *_memset - fills memory with a constant byte
+ * @s: memory area to be filled
+ * @b: char to copy
+ * @n: number of times to copy b
+ *
+ * Return: pointer to the memory area s
  */
-
 char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i;
 
-	for (i = 0; i < 1; i++)
+	for (i = 0; i < n; i++)
 	{
 		s[i] = b;
 	}
