0f596f59e3b556fe70fdc5141e49bf402a4e8f8c
emmyfaculty <emmanuelahola91@gmail.com>
2021-09-24 22:42:00 +0000

    error

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,12 +1,13 @@
-/*
- * File: 101-mul.c
- * Auth: Ahola Emmanuel
- */
-
-#include "main.h"
 #include <stdlib.h>
 #include <stdio.h>
+#include "main.h"
 
+/**
+ * is_digit - checks if a string contains a non-digit char
+ * @s: string to be evaluated
+ *
+ * Return: 0 if a non-digit is found, 1 otherwise
+ */
 int is_digit(char *s)
 {
 	int i = 0;
