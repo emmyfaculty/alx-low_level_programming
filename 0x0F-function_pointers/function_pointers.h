1e1921aff581735856362ad5cbc5baa21c5c9375
emmyfaculty <emmanuelahola91@gmail.com>
2021-09-30 16:55:19 +0000

    committing pointer functions

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,10 +1,10 @@
-#ifndef FUNC_H
-#define FUNC_H
-#include <stdlib.h>
+#ifndef POINTERS_FUNTIONS
+#define POINTERS_FUNTIONS
 
+#include <stdlib.h>
+#include <stddef.h>
+int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
-
 #endif
-© 2
