65a3b454ea471317a877e56c1d08afdf12cb9c16
emmyfaculty <emmanuelahola91@gmail.com>
2021-10-13 18:57:24 +0000

    correction

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -1,24 +1,20 @@
+#include <stdlib.h>
 #include "lists.h"
 
 /**
-  * free_list - fills memory with a constant byte
-  * @head: is the owner of the dog
-  * Return: a number
-  */
+ * free_list - frees a linked list
+ * @head: list_t list to be freed
+ */
 void free_list(list_t *head)
 {
-	list_t *savepoin;
+	list_t *temp;
 
-	if (head != NULL)
+	while (head)
 	{
-		while (head->next != NULL)
-		{
-			savepoin = head->next;
-			free(head->str);
-			free(head);
-			head = savepoin;
-		}
+		temp = head->next;
 		free(head->str);
 		free(head);
+		head = temp;
 	}
 }
+
