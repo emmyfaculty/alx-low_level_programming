376fc0f4f838e0fc6084c058fd7892c1515e8b1c
emmyfaculty <emmanuelahola91@gmail.com>
2021-10-13 18:38:45 +0000

    corrections

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,41 +1,43 @@
-#include <stdlib.h>
-#include <string.h>
 #include "lists.h"
 
 /**
- * add_node_end - adds a new node at the end of a linked list
- * @head: double pointer to the list_t list
- * @str: string to put in the new node
- *
- * Return: address of the new element, or NULL if it failed
- */
+  * add_node_end - fills memory with a constant byte
+  * @head: is the owner of the dog
+  * @str: is a string
+  * Return: a address
+  */
 list_t *add_node_end(list_t **head, const char *str)
 {
-	list_t *new;
-	list_t *temp = *head;
-	unsigned int len = 0;
+	list_t *new, *headcopy;
+	size_t n;
 
-	while (str[len])
-		len++;
+	headcopy = *head;
 
 	new = malloc(sizeof(list_t));
-	if (!new)
+	if (new == NULL)
 		return (NULL);
-
 	new->str = strdup(str);
-	new->len = len;
-	new->next = NULL;
-
+	if (new->str == NULL)
+	{
+		free(new);
+		return (NULL);
+	}
+	for (n = 0; new->str[n] != '\0'; n++)
+		;
+	new->len = n;
 	if (*head == NULL)
 	{
+		new->next = *head;
 		*head = new;
-		return (new);
+	}
+	else
+	{
+		while (headcopy->next != NULL)
+			headcopy = headcopy->next;
+		new->next = (headcopy)->next;
+		(headcopy)->next = new;
 	}
 
-	while (temp->next)
-		temp = temp->next;
-
-	temp->next = new;
-
-	return (new);
+	return (*head);
 }
+
